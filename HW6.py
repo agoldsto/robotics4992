dbef99eda41b25c720970cfa9d47f4bde0a1d5a6
agoldsto <agoldsto@masonlive.gmu.edu>
2015-04-02 13:16:53 -0400

    HW6

---
+++ HW6.py
@@ -0,0 +1,37 @@
+import serial
+import socket
+import sys
+import time
+def com(port=2222,dev="/dev/ttyACM0"):
+	s=socket.socket()
+	s.bind(('',int(port)))
+	s.listen(1)
+	cs,ca=s.accept()
+	ser=serial.Serial(str(dev),9600)
+	encodeA=lambda v :hex(int(v)%160)[2:].decode('hex')
+	a='P'
+	try:
+		while(1):
+			cmd=cs.recv(3)
+			print 'cmd: '+str(cmd)
+			try:
+				a=encodeA(cmd)
+				#a=int(cmd)%160
+			except:
+				pass
+			ser.write(a)
+			print 'sent: ' + str(a)
+			print 'recieved: '+ser.readline()
+			#if a=='P':
+				#time.sleep(.1)
+	finally:
+		s.close()
+		cs.close()
+
+if __name__ =="__main__":
+    if len(sys.argv)==3:
+    	com(sys.argv[1],sys.argv[2])
+    elif len(sys.argv)==2:
+    	com(sys.argv[1])
+    else:
+    	com()
---
+++ vt.py
@@ -0,0 +1,64 @@
+import numpy as np
+import cv2
+import time
+#import serial
+#ser=serial.Serial("/dev/ttyACM0",9600)
+import socket
+s=socket.socket()
+s.connect(("192.168.1.244",2222))
+time.sleep(1)
+cap = cv2.VideoCapture(0)
+cap.set(3,160)#640
+cap.set(4,120)#480
+hasblue=lambda mask: any(map(any,mask))
+while(True):
+	# Capture frame-by-frame
+	ret, frame = cap.read()
+	# Display the resulting frame
+	# Convert BGR to HSV
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # define range of blue color in HSV
+    lower_blue = np.array([110, 50, 50], dtype=np.uint8)
+    upper_blue = np.array([130,255,255], dtype=np.uint8)
+
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    xs=[]
+    ys=[]
+    if mask.any():
+    	for i in range(len(mask)):
+			if any(mask[i]):
+				for j in range(len(mask[i])):
+					if mask[i][j]==255:
+						ys.append(i)
+						xs.append(j)
+    xa=sum(xs)/(len(xs)+1)
+    ya=sum(ys)/(len(ys)+1)
+    p=(xa,ya)
+    #time.sleep(.25)
+    s.sendall(str(xa))
+    #serport="/dev/ttyACM0"
+    #ser=serial.Serial(serport,9600)
+    #ser.write(str(xa))
+    
+    r,framedot=cap.read()
+    maskbgr=cv2.cvtColor(mask,cv2.COLOR_GRAY2BGR)
+    cv2.circle(maskbgr,p,5,[255,0,0],5)
+    cv2.circle(framedot,p,5,[255,0,0],5)
+    print p
+    # Bitwise-AND mask and original image
+#    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    #cv2.imshow('frame',frame)
+    #cv2.imshow('framedot',framedot)
+    #cv2.imshow('mask',mask)
+    cv2.imshow('maskbgr',maskbgr)
+#    cv2.imshow('res',res)
+
+    #cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+       break
+
+   
